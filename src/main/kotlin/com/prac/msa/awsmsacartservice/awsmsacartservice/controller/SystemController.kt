5ab180494b1758ccf38ff90b5f7fd703cdb191e0
burger2020 <blac2013@gmail.com>
2023-07-24 15:29:06 +0900

    health_check uri 변경

--- src/main/kotlin/com/prac/msa/awsmsacartservice/awsmsacartservice/controller/SystemController.kt
+++ src/main/kotlin/com/prac/msa/awsmsacartservice/awsmsacartservice/controller/SystemController.kt
@@ -4,12 +4,14 @@ import com.sun.management.OperatingSystemMXBean
 import org.springframework.http.ResponseEntity
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.lang.management.ManagementFactory
 import java.net.InetAddress
 import java.time.LocalDateTime
 
 @RestController
+@RequestMapping("/api/carts")
 class SystemController {
 
     @GetMapping("/health_check")
